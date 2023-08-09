be2ce56640b66245f93ae80d1ab0cc5312001e48
Cecilia <45336766+ceciliadean182@users.noreply.github.com>
2023-08-09 21:22:08 +0000

    Change color button

--- src/js/component/trafficLight.js
+++ src/js/component/trafficLight.js
@@ -23,9 +23,23 @@ const TrafficLight = () => {
         setYellow("bg-warning");
     }
 
+    function changeColor() {
+        if (red === "bg-danger" && yellow === "bg-warning" && green === "bg-success") { // cuando ninguna luz brillando
+            handleColorRed(); // iluminamos por defecto la primera
+        }
+        if (red === "bg-danger glow") {
+            handleColorYellow();
+        } else if (yellow === "bg-warning glow") {
+            handleColorGreen();
+        } else if (green === "bg-success glow") {
+            handleColorRed();
+        }
+    }
+
     return (
         <div className="text-center container">
 			<h1 className="text-center mt-5">Traffic Light</h1>
+            <button className="btn btn-secondary mt-4" onClick={changeColor}>Cambiar color</button>
             <div className="container text-center bg-dark mt-5" style={{ height: '290px', width: '150px' }}>
                 <div className="row">
                     <div className="col d-flex justify-content-center align-items-center">
